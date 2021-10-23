558d0f4984dc99ab3626f46b683ed7329667a89e
Owain Jones <contact@odj.me>
2021-10-23 04:37:58 +0100

    improved readability, improved bumbleline.js, wrote today's post

--- static/bumbleline.js
+++ static/bumbleline.js
@@ -1,9 +1,60 @@
-function bumbleline (root) {
-	var STEP = 5;
-	var z_incr = -STEP;
-	var start_z = 150;
-	var end_z = 255;
-	var z = end_z;
+function _bumbleline(root, z, min_z, max_z, z_step, replace_color) {
+	var step = z_step;
+	var start_z = z;
+	var childNodes = []
+	for(var n=0; n<root.childNodes.length; n++) {
+		childNodes.push(root.childNodes[n]);
+	}
+	for(var n=0; n<childNodes.length; n++) {
+		var node = childNodes[n];
+		if(['TITLE', 'SCRIPT', 'IMG'].indexOf(node.nodeName) > -1) {
+			continue;
+		}
+		if(['DIV', 'P', 'UL', 'LI'].indexOf(node.nodeName) > -1) {
+			z = start_z;
+			step = z_step;
+		}
+		if(node.nodeName != '#text') {
+			ret = _bumbleline(node, z, min_z, max_z, step, replace_color);
+			root.insertBefore(document.createTextNode(' '), node);
+			z = ret[0];
+			step = ret[1];
+			continue;
+		}
+		var s = window.getComputedStyle(node.parentNode);
+		if (s.color != replace_color) {
+			continue;
+		}
+		var words = node.nodeValue.split(/(\s+)/);
+		var text='';
+		for(var w=0; w<words.length; w++) {
+			var word = words[w];
+			text += word;
+			if(word.match(/\S+/) == null) {
+				continue;
+			}
+			z += step;
+			if (z >= max_z) {
+				z = max_z;
+				step = -step;
+			}
+			if (z <= min_z) {
+				z = min_z;
+				step = -step;
+			}
+			var elem = document.createElement('span');
+			elem.innerHTML = text;
+			elem.style.color = 'rgb(' + z + ', ' + z + ', ' + z + ')';
+			root.insertBefore(elem, node);
+			text = '';
+		}
+		root.insertBefore(document.createTextNode(''), node);
+		root.removeChild(node);
+	}
+	return [z, step];
+}
+
+function bumbleline (root, z, min_z, max_z, z_step, replace_color) {
 	root.originalHTML = root.cloneNode(true).innerHTML;
 	root.swap = function () {
 		if (root.classList.contains('beeline')) {
@@ -14,52 +65,17 @@ function bumbleline (root) {
 			root.classList.add('beeline');
 		}
 	};
-	var paragraphs = root.getElementsByTagName('p');
-	for(var i=0; i<paragraphs.length; i++) {
-		z = end_z;
-		z_incr = -STEP;
-		var p = paragraphs[i];
-		var s = window.getComputedStyle(p);
-		if (s.color != 'rgb(204, 204, 204)') {
-			continue;
-		}
-		var cn = p.childNodes;
-		for(var x=0; x<cn.length; x++) {
-			var c = cn[x];
-			if(c.nodeName != '#text') {
-				continue;
-			}
-			var words = c.nodeValue.split(/(\s+)/);
-			var text = ''
-			for(var w=0; w<words.length; w++) {
-				var word = words[w];
-				text += word;
-				if (word.match(/\S+/) == null) {
-					continue;
-				}
-				z += z_incr;
-				if (z >= end_z) {
-					z = end_z;
-					z_incr = -STEP;
-				}
-				if (z <= start_z) {
-					z = start_z;
-					z_incr = STEP;
-				}
-				var word_elem = document.createElement('span');
-				word_elem.innerHTML = text;
-				word_elem.style.color = 'rgb(' + z + ', ' + z + ', ' + z + ')';
-				p.insertBefore(word_elem, c);
-				text = '';
-			}
-			p.insertBefore(document.createTextNode(' '), c);
-			p.removeChild(c);
-		}
-	}
+	_bumbleline(root, z, min_z, max_z, z_step, replace_color);
 	root.beelineHTML = root.cloneNode(true).innerHTML;
 	root.classList.add('beeline');
-};
+}
 
 document.addEventListener('DOMContentLoaded', function(event) {
-	bumbleline(document.getElementById('article'));
+	var root = document.getElementById('article');
+	var z_step = -2;
+	var min_z = 160;
+	var max_z = 240;
+	var z = max_z;
+	var replace_color = 'rgb(204, 204, 204)';
+	bumbleline(root, z, min_z, max_z, z_step, replace_color);
 });
